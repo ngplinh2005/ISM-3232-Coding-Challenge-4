8198d891bf5cf3128967f8599969435ba9a378ea
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-09-14 19:15:06 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,33 @@
+// Task 1: Create an Inventory Array of Product Objects
+let inventory = [
+    {
+        name: "Laptop",
+        price: 99.00,
+        quantity: 30,
+        lowStockLevel: 10
+    },
+    {
+        name: "Book",   
+        price: 50.00,
+        quantity: 200,
+        lowStockLevel: 60
+    },
+    {
+        name: "Bed",
+        price: 999.98,
+        quantity: 50,
+        lowStockLevel: 5
+    },
+    {
+        name: "Shoes",
+        price: 40,
+        quantity: 150,
+        lowStockLevel: 100
+    },
+    {
+        name: "Table",
+        price: 80,
+        quantity: 72,
+        lowStockLevel: 20
+    }
+]
